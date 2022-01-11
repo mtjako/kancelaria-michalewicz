2db16dfaca32c4fb0154b84f653bacc9d2a0a29a
Mateusz Jakowlew <mtjako@mtjako-macbook.local>
2022-01-11 13:02:10 +0100

    added contact section

---
+++ components/contact.php
@@ -0,0 +1,23 @@
+<div class="contact">
+    <div class="contact__wrap">
+        <div class="contact__text">
+            <h3 class="contact__text__title">Kontakt</h3>
+            <p class="contact__text__description">W celu umówienia się na poradę prawną lub uzyskania szczegółów dotyczących prowadzenia sprawy należy skontaktować się mailowo bądź telefonicznie celem omówienia szczegółów lub uzyskania pomocy prawnej. </p>
+            <div class="contact__text__info">
+                <img src="<?php echo get_template_directory_uri().'/assets/icons/phone.svg' ?>" alt="phone"><p>+48 605 077 013</p>
+            </div>
+            <div class="contact__text__info">
+                <img src="<?php echo get_template_directory_uri().'/assets/icons/mail.svg' ?>" alt="mail"><p>adwokatmichalewicz@gmail.com</p>
+            </div>
+            <div class="contact__text__info">
+                <img src="<?php echo get_template_directory_uri().'/assets/icons/card.svg' ?>" alt="card"><p>ING S.A. 06 1050 1575 1000 0092 6973 6931</p>
+            </div>
+            <div class="contact__text__info">
+                <img src="<?php echo get_template_directory_uri().'/assets/icons/map.svg' ?>" alt="map"><p>ul. Kawalerzystów 2/12 <br/> 53-004 Wrocław</p>
+            </div>
+        </div>
+        <div class="contact__img">
+            <img src="<?php echo get_template_directory_uri().'/assets/img/building.jpg'?>" alt="Agata Michalewicz Office">
+        </div>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 <?php get_template_part('components/hero'); ?>
 <?php get_template_part('components/features'); ?>
 <?php get_template_part('components/about'); ?>
+<?php get_template_part('components/contact'); ?>
 <?php get_footer() ?>
