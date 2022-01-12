11b0436a453eb1207f4d7db507d7a67d44d1b74d
Mateusz Jakowlew <mtjako@mtjako-macbook.local>
2022-01-12 16:17:34 +0100

    added blog features and bug fixes

---
+++ components/hero-post.php
@@ -0,0 +1,17 @@
+<div class="hero" style="background-image: url('<?php the_post_thumbnail_url() ?>')">
+    <div class="hero__overlay">
+    <div class="hero__header">
+        <h1><?php the_title()?></h1>
+        <div class="hero__header__meta">
+            <p><?php echo get_the_date()?></p>
+            <div class="hero__header__tags">
+                <?php the_tags( 
+                    '<span class="hero__header__tag">#', 
+                    '</span><span class="hero__header__tag">#', 
+                    '</span>'
+                )?>
+            </div>
+        </div>
+    </div>
+    </div>
+</div>
