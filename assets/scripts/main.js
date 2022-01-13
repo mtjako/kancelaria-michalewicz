448047c0ce569a70ef51d678cef04534f1d86a81
Mateusz Jakowlew <mtjako@mtjako-macbook.local>
2022-01-13 15:47:14 +0100

    bug fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,15 @@
 //hamburger menu
 const btn = document.querySelector(".navigation__wrap__btn");
 const menu = document.querySelector(".navigation__wrap__menu");
+const item = document.querySelectorAll(".navigation__wrap__menu ul li a");
 btn.addEventListener("click", () => {
-  console.log("click");
   menu.classList.toggle("open");
+});
+
+item.forEach((i) => {
+  i.addEventListener("click", () => {
+    menu.classList.toggle("open");
+  });
 });
 
 //footer year
